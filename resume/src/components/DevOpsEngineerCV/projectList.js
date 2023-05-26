019fa803c0cce546a4bae249a5a7945f73eaee88
tush-tr <tusharrajput604@gmail.com>
2023-05-26 23:10:52 +0530

    new project

--- resume/src/components/DevOpsEngineerCV/projectList.js
+++ resume/src/components/DevOpsEngineerCV/projectList.js
@@ -1,27 +1,27 @@
 const projectsList = [
-    {
-      title: "Gcloudact",
-      desc: "A Terraform module for hosting your own runner for CI/CD on Google Cloud to run jobs in your GitHub Actions workflows.",
-      link: "https://github.com/tush-tr/gcloudact",
-      liveLink: null,
-    },
-    {
-      title: "Runode",
-      desc: "A CLI tool for running nodejs server automatically in development mode.",
-      link: "https://www.npmjs.com/package/runode",
-      liveLink: null,
-    },
-    {
-        title: "CI/CD with Terraform and AWS",
-        desc: "A node.js app deployed to Amazon EC2 using terraform, docker, Amazon ECR, Github Actions workflow.",
-        link: "https://github.com/kubekode/DevOps-Projects/tree/Complete-CI/CD-with-Terraform-AWS",
-        liveLink: null,
-      },    {
-        title: "CI/CD with Terraform and GKE",
-        desc: "A node.js app deployed to GKE using terraform, docker, Kubernetes and Github Actions workflow.",
-        link: "https://github.com/KubeKode/DevOps-Projects/tree/Complete-CI/CD-with-Terraform-GKE",
-        liveLink: null,
-      },
-  ];
-  export default projectsList;
-  
+  {
+    title: "Gcloudact",
+    desc: "A Terraform module for hosting your own runner for CI/CD on Google Cloud to run jobs in your GitHub Actions workflows.",
+    link: "https://github.com/tush-tr/gcloudact",
+    liveLink: null,
+  },
+  {
+    title: "VMdeploy",
+    desc: "A GitOps Continuous Delivery Tool for VMs.",
+    link: "https://www.npmjs.com/package/vmdeploy",
+    liveLink: null,
+  },
+  {
+    title: "CI/CD with Terraform and AWS",
+    desc: "A node.js app deployed to Amazon EC2 using terraform, docker, Amazon ECR, Github Actions workflow.",
+    link: "https://github.com/kubekode/DevOps-Projects/tree/Complete-CI/CD-with-Terraform-AWS",
+    liveLink: null,
+  },
+  {
+    title: "CI/CD with Terraform and GKE",
+    desc: "A node.js app deployed to GKE using terraform, docker, Kubernetes and Github Actions workflow.",
+    link: "https://github.com/KubeKode/DevOps-Projects/tree/Complete-CI/CD-with-Terraform-GKE",
+    liveLink: null,
+  },
+];
+export default projectsList;
